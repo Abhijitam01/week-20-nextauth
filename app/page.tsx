a486981af9576cdb2f65ced56f99911265884b0a
unknown <abhijitamdubey31@gmail.com>
2025-03-10 03:49:29 +0530

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
         <Image
           className="dark:invert"
           src="/next.svg"
-          alt="Next.js logo"
+          alt="Next.js   logo"
           width={180}
-          height={38}
+          height={38} 
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
